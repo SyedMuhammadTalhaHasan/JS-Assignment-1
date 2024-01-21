cb4ce2fc74b2d78ec1ee67d89ac2eac0de8874dd
SyedMuhammadTalhaHasan <155646073+SyedMuhammadTalhaHasan@users.noreply.github.com>
2024-01-21 11:01:08 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var age = 18;
+var isStudent = "student";
+console.log("My age is " + age);
+console.log("I'm a " + isStudent);
+
+alert("Hi, My name is Syed Muhammad Talha Hasan, I'm a student of website designing and development. I want to personally welcome you to my website.")
